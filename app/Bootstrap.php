ae00d463844b6cc88ba5c52ac0e74f3d206912d7
Basil Tatarnikov <tatvasily@gmail.com>
2023-04-19 12:00:50 +0800

    feat: добавлены инициализация IoC-контейнера и базовый скрипт для запуска сортировки

---
+++ app/Bootstrap.php
@@ -0,0 +1,66 @@
+<?php
+
+use App\GameObject;
+use App\IO\File\FileReadCommand;
+use App\IO\File\FileWriteCommand;
+use App\IoC\IoC;
+use App\MacroCommand;
+use App\Sort\FileSortCommand;
+use App\Sort\InsertionSortCommand;
+use App\Sort\MergeSortCommand;
+use App\Sort\SelectionSortCommand;
+use App\Sort\SortableAdapter;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+$container = new IoC;
+
+$container->resolve(
+    'IoC.Register',
+    'Sortable',
+    fn (...$attrs) => new SortableAdapter(new GameObject)
+)->execute();
+
+$container->resolve(
+    'IoC.Register',
+    'Command.Sort.Insertion',
+    fn (...$attrs) => new InsertionSortCommand($attrs[0])
+)->execute();
+
+$container->resolve(
+    'IoC.Register',
+    'Command.Sort.Merge',
+    fn (...$attrs) => new MergeSortCommand($attrs[0])
+)->execute();
+
+$container->resolve(
+    'IoC.Register',
+    'Command.Sort.Selection',
+    fn (...$attrs) => new SelectionSortCommand($attrs[0])
+)->execute();
+
+$container->resolve(
+    'IoC.Register',
+    'IO.File.Read',
+    fn (...$attrs) => new FileReadCommand($attrs[0], $attrs[1])
+)->execute();
+
+$container->resolve(
+    'IoC.Register',
+    'IO.File.Write',
+    fn (...$attrs) => new FileWriteCommand($attrs[0], $attrs[1])
+)->execute();
+
+$container->resolve(
+    'IoC.Register',
+    'SortFile',
+    function (string $inputPath, string $outputPath, string $sortCommand) use ($container) {
+        $container->resolve('IoC.Register', 'Sort.File.Input', fn (...$atrs) => $inputPath)->execute();
+        $container->resolve('IoC.Register', 'Sort.File.Output', fn (...$atrs) => $outputPath)->execute();
+        $container->resolve('IoC.Register', 'Sort.Type', fn (...$atrs) => $sortCommand)->execute();
+
+        $fileSortCommand = new FileSortCommand($container);
+        $fileSortCommand->execute();
+    }
+)->execute();
+
+return $container;
